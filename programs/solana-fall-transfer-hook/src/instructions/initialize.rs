2f2a74cfb1cb97c28676f9f2630cf00d877f3792
initiate <76862995+init-ikuya@users.noreply.github.com>
2026-09-14 21:26:55 +0900

    challenge 1: validate the mint is owned by Token-2022

--- programs/solana-fall-transfer-hook/src/instructions/initialize.rs
+++ programs/solana-fall-transfer-hook/src/instructions/initialize.rs
@@ -1,5 +1,5 @@
 use anchor_lang::prelude::*;
-use anchor_spl::{token_2022, token_interface::Mint};
+use anchor_spl::{token_2022::Token2022, token_interface::Mint};
 
 use crate::{ANCHOR_DISCRIMINATOR_SIZE, RateLimit, error::ErrorCode};
 
@@ -7,6 +7,7 @@ use crate::{ANCHOR_DISCRIMINATOR_SIZE, RateLimit, error::ErrorCode};
 pub struct Initialize<'info> {
     #[account(mut)]
     pub payer: Signer<'info>,
+    pub mint: InterfaceAccount<'info, Mint>,
     #[account(
         init,
         payer = payer,
@@ -21,8 +22,7 @@ pub struct Initialize<'info> {
 }
 
 pub fn handler(ctx: Context<Initialize>) -> Result<()> {
-    // For the challenge - Ensure the mint is a token-2022 mint by checking its owner (Pass the mint in the context and check its owner. 
-    // Consider saving the mint in the RateLimit struct if needed for future use.
+    require_keys_eq!(*ctx.accounts.mint.to_account_info().owner, Token2022::id(), ErrorCode::InvalidMint);
 
     // Initialize the rate limit account with the authority, mint, max amount, and window start timestamp
     ctx.accounts.rate_limit.set_inner(RateLimit {
--- programs/solana-fall-transfer-hook/tests/helpers/mod.rs
+++ programs/solana-fall-transfer-hook/tests/helpers/mod.rs
@@ -54,6 +54,7 @@ pub fn initialize_mint(svm: &mut LiteSVM, payer: &Keypair, mint: &Keypair, progr
 
 // For the challenge - Initialize the rate limit account and the extra account meta list for a given mint
 pub fn initialize_rate_limit(svm: &mut LiteSVM, payer: &Keypair, mint: &Keypair, program_id: &Address) {
+    
     let rate_limit = Pubkey::find_program_address(
         &[b"rate_limit"],
         program_id,
@@ -64,6 +65,7 @@ pub fn initialize_rate_limit(svm: &mut LiteSVM, payer: &Keypair, mint: &Keypair,
         &solana_fall_transfer_hook::instruction::Initialize {}.data(),
         solana_fall_transfer_hook::accounts::Initialize {
             payer: payer.pubkey(),
+            mint: mint.pubkey(),
             rate_limit,
             system_program: SYSTEM_PROGRAM_ID,
         }.to_account_metas(None),
